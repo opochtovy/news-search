7847945d5fa844683eab1f260d56b2780e62f483
Oleg Pochtovy <opochtovy@yahoo.com>
2016-02-02 16:11:28 +0300

    ITBAccessToken creation

---
+++ NewsSearch/NewsSearch/ITBAccessToken.h
@@ -0,0 +1,13 @@
+//
+//  ITBAccessToken.h
+//  NewsSearch
+//
+//  Created by Oleg Pochtovy on 02.02.16.
+//  Copyright © 2016 Oleg Pochtovy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ITBAccessToken : NSObject
+
+@end
---
+++ NewsSearch/NewsSearch/ITBAccessToken.m
@@ -0,0 +1,13 @@
+//
+//  ITBAccessToken.m
+//  NewsSearch
+//
+//  Created by Oleg Pochtovy on 02.02.16.
+//  Copyright © 2016 Oleg Pochtovy. All rights reserved.
+//
+
+#import "ITBAccessToken.h"
+
+@implementation ITBAccessToken
+
+@end
