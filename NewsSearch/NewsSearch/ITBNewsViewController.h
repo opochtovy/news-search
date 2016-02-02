cbb83c4205fa209dc7a3f5222e68729f4d641737
Oleg Pochtovy <opochtovy@yahoo.com>
2016-02-02 16:27:37 +0300

    Main screen VC creation

--- NewsSearch/NewsSearch/ITBNewsViewController.h
+++ NewsSearch/NewsSearch/ITBNewsViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  ITBNewsViewController.h
 //  NewsSearch
 //
 //  Created by Oleg Pochtovy on 02.02.16.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface ITBNewsViewController : UITableViewController
 
 @end
-
